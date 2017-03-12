72e15ed25b92b36c1d0bd64a27f72e2a3fd4c7d1
Auriles El Haddad <auriles0404@gmail.com>
2017-03-12 20:16:15 +0100

    Ajouter un item

--- admin/insert.php
+++ admin/insert.php
@@ -2,6 +2,94 @@
 
   require 'database.php';
 
+  $nameError = $descriptionError = $priceError = $categoryError = $imageError = $name = $description = $price = $category = $image = "";
+
+  if(!empty($_POST)) {
+
+    $name            = checkInput($_POST['name']);
+    $description     = checkInput($_POST['description']);
+    $price           = checkInput($_POST['price']);
+    $category        = checkInput($_POST['category']);
+    $image           = checkInput($_FILES['image']['name']);
+    $imagePath       = '../images/' . basename($image);
+    $imageExtension  = pathinfo($imagePath, PATHINFO_EXTENSION);
+    $isSuccess       = true;
+    $isUploadSuccess = false;
+
+    // Nom
+    if(empty($name)) {
+      $nameError = 'Ce champ ne peut pas être vide';
+      $isSuccess = false;
+    }
+
+    // Description
+    if(empty($description)) {
+      $descriptionError = 'Ce champ ne peut pas être vide';
+      $isSuccess = false;
+    }
+
+    // Prix
+    if(empty($price)) {
+      $priceError = 'Ce champ ne peut pas être vide';
+      $isSuccess = false;
+    }
+
+    // Catégorie
+    if(empty($category)) {
+      $categoryError = 'Ce champ ne peut pas être vide';
+      $isSuccess = false;
+    }
+
+    // Image
+    if(empty($image)) {
+      $imageError = 'Ce champ ne peut pas être vide';
+      $isSuccess = false;
+
+    } else {
+
+      $isUploadSuccess = true;
+
+      if($imageExtension != "jpg" && $imageExtension != "png" && $imageExtension != "jpeg" && $imageExtension != "gif" ) {
+          $imageError = "Les fichiers autorisés sont: .jpg, .jpeg, .png, .gif";
+          $isUploadSuccess = false;
+      }
+
+      if(file_exists($imagePath)) {
+          $imageError = "Le fichier existe déjà";
+          $isUploadSuccess = false;
+      }
+
+      if($_FILES["image"]["size"] > 500000) {
+          $imageError = "Le fichier ne doit pas dépasser les 500KB";
+          $isUploadSuccess = false;
+      }
+
+      if($isUploadSuccess) {
+          if(!move_uploaded_file($_FILES["image"]["tmp_name"], $imagePath))
+          {
+              $imageError = "Il y a eu une erreur lors de l'upload";
+              $isUploadSuccess = false;
+          }
+      }
+    }
+
+    if($isSuccess && $isUploadSuccess) {
+      $db = Database::connect();
+      $statement = $db->prepare('INSERT INTO items (name,description,price,category,image) values(?, ?, ?, ?, ?)');
+      $statement->execute(array($name,$description,$price,$category,$image));
+      Database::diconnect();
+      header("Location: index.php");
+    }
+
+  }
+
+  function checkInput($data) {
+    $data = trim($data);
+    $data = stripcslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -20,7 +108,6 @@
   </head>
 
   <body>
-
     <h1 class="text-logo"><span class="glyphicon glyphicon-cutlery"></span> Burger Code <span class="glyphicon glyphicon-cutlery"></span></h1>
     <div class="container admin">
         <div class="row">
@@ -29,25 +116,25 @@
           <form class="form" role="form" action="insert.php" method="post" enctype="multipart/form-data">
             <!-- Nom -->
             <div class="form-group">
-              <label for="name">Nom: </label>
+              <label for="name">Nom : </label>
               <input type="text" class="form-control" id="name" name="name" placeholder="Nom" value="<?php echo $name; ?>">
               <span class="help-inline"><?php echo $nameError; ?></span>
             </div>
             <!-- Description -->
             <div class="form-group">
-              <label for="description">Description: </label>
+              <label for="description">Description : </label>
               <input type="text" class="form-control" id="description" name="description" placeholder="Description" value="<?php echo $description; ?>">
               <span class="help-inline"><?php echo $descriptionError; ?></span>
             </div>
             <!-- Prix -->
             <div class="form-group">
-              <label for="price">Prix: (en €) </label>
+              <label for="price">Prix : (en €) </label>
               <input type="number" step="0.01" class="form-control" id="price" name="price" placeholder="Prix" value="<?php echo $price; ?>">
               <span class="help-inline"><?php echo $priceError; ?></span>
             </div>
             <!-- Catégorie -->
             <div class="form-group">
-              <label for="category">Catégorie: </label>
+              <label for="category">Catégorie : </label>
               <select name="category" id="category" class="form-control">
                 <?php
 
@@ -55,29 +142,25 @@
                   foreach($db->query('SELECT * FROM categories') AS $row) {
                     echo '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
                   }
-                  $Database::disconnect();
+                  Database::disconnect();
                 ?>
               </select>
               <span class="help-inline"><?php echo $categoryError; ?></span>
             </div>
             <!-- Image -->
             <div class="form-group">
-              <label for="image">Sélectionner une image:</label>
+              <label for="image">Sélectionner une image :</label>
               <input type="file" id="image" name="image">
               <span class="help-inline"><?php echo $imageError; ?></span>
             </div>
+            <br>
+            <div class="form-actions">
+              <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-pencil"></span> Ajouter</button>
+              <a href="index.php" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Retour</a>
+            </div>
           </form>
-          <br>
-          <div class="form-actions">
-            <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-pencil"></span> Ajouter</button>
-            <a href="index.php" class="btn btn-primary"><span class="glyphicon glyphicon-arrow-left"></span> Retour</a>
-          </div>
-
-
-
         </div>
     </div>
-
   </body>
 
 </html>

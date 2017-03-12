07b6a3865adfc60a2177cf1694934b197b1507db
Auriles El Haddad <auriles0404@gmail.com>
2017-03-12 16:56:28 +0100

    Liste des items

--- admin/database.php
+++ admin/database.php
@@ -2,29 +2,29 @@
 
   class Database {
 
-    private static $dbHost = "localhost";
-    private static $dbName = "burger_code";
-    private static $dbUser = "root";
-    private static $dbUserPassword = "";
+      private static $dbHost = "localhost";
+      private static $dbName = "burger_code";
+      private static $dbUsername = "root";
+      private static $dbUserpassword = "";
 
-    private static $connection = null;
+      private static $connection = null;
 
-    public static function connect() {
-      try {
-        self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbaname=" . self::$dbName, self::$dbUser, self::$dbUserPassword);
-      } catch(PDOException $e) {
-        die($e->getMessage());
+      public static function connect() {
+
+          if(self::$connection == null) {
+              try {
+                self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName , self::$dbUsername, self::$dbUserpassword);
+              } catch(PDOException $e) {
+                  die($e->getMessage());
+              }
+          }
+          return self::$connection;
       }
-      return self::$connection;
-    }
 
-    public static function disconnect() {
-      self::$connection = null;
-    }
+      public static function disconnect() {
+          self::$connection = null;
+      }
 
   }
-
-  Database::connect();
-
 
 ?>
